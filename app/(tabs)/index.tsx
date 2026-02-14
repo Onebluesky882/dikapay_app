873728e6069d545738c3488d325d997b250cb94d
Onebluesky882 <onebluesky882@outlook.com>
2026-02-14 16:03:20 +0700

    reinstall

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <View className="bg-red-100 flex-1   justify-center items-center">
-      <Text className="text-red-500"> hello</Text>
-    </View>
-  );
-}
