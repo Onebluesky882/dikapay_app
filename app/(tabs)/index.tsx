6f535ad1922d3f4b3d4b2920661f632685bd1856
Onebluesky882 <onebluesky882@outlook.com>
2026-02-14 16:12:58 +0700

    build : starter with uniwind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-blue-500">index</Text>
+    </View>
+  );
+}
