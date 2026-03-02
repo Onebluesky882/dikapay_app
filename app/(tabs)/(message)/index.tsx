67425dca59160563cd67235b1e28d735d57b1d24
Onebluesky882 <onebluesky882@outlook.com>
2026-03-02 18:16:17 +0700

    build : profile page

--- app/(tabs)/(message)/index.tsx
+++ app/(tabs)/(message)/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function search() {
+export default function message() {
   return (
     <SafeAreaView>
       <View>
